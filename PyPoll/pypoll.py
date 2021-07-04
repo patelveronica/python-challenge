673e329c8ed9d9af4eadc7a3cdd93f028e2f333f
patelveronica <85206817+patelveronica@users.noreply.github.com>
2021-07-04 11:37:13 -0400

    added winner and wrote the results in summary text file

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -14,13 +14,13 @@ OTooley_votes = 0
 # Method 2: Improved Reading using CSV module
 
 with open(csvpath) as csvfile:
-
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    # print(csvreader)
+    print(csvreader)
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
+    # loop thru every row
+    row = next(csvreader)
     #for loop to count the total vot and sum the total votes.
     for row in csvreader:
         #count total votes and sum
@@ -34,15 +34,13 @@ with open(csvpath) as csvfile:
             Li_votes += 1
         elif row[2] == "O'Tooley":
             OTooley_votes += 1
-        # #maybe i should add function here:
-    # def winnername(candidates):
-    #     if Khan_votes > Correy_votes:
-            # elif Khan_votes > Li_votes:
-            #     elif Khan_votes > OTooley_votes:
-# if khan_percentage > Correy_percentage and khan_percentage > Li_percentage and khan_percentage > OTooley_percentage:
-candidates = ["Khan", "Correy","Li", "O'Tooley" ]
-#store candidates votes    
-Votes = [Khan_votes, Correy_votes, Li_votes, OTooley_votes]
+# create dictionary for candiates name and their total votes        
+candidates = ["Khan", "Correy","Li", "O'Tooley"]   
+candidates_votes = [Khan_votes, Correy_votes, Li_votes, OTooley_votes]
+# We the list of candidate(key) and the total votes(value) together and using max function, print the winner
+candidates_and_votes_dict = dict(zip(candidates,candidates_votes))
+winner = max(candidates_and_votes_dict, key=candidates_and_votes_dict.get)
+
 #Calculate the percentage for each candidate
 khan_percentage = (Khan_votes/total_votes)
 Correy_percentage = (Correy_votes/total_votes)
@@ -59,27 +57,21 @@ print("Correy: "+"{:.3%}".format(Correy_percentage) + " " + str(Correy_votes))
 print("Li: "+"{:.3%}".format(Li_percentage) + " " + str(Li_votes))
 print("O'Tooley: "+"{:.3%}".format(OTooley_percentage) + " " + str(OTooley_votes))
 print("-------------------------------------------")
-print("Winner: " + "Khan")
-# print("The Winner is: " + winnername[0])
+print(f"Winner: {winner}")
 print("-------------------------------------------")
 
 # Save the summary results to textfile
 save_textfile = csvpath.strip(".csv") + "_ElectionResults.txt"
-csvpath = os.path.join('Resources', 'election_data.csv')
-with open(csvpath, 'w')as result:
-    print("Election Results")
-result.write("Election Results" + "\n")
-result.write("-------------------------------------------" + "\n")
-result.write(f"Total Votes: {str(total_votes)}" + "\n")
-result.write("-------------------------------------------" + "\n")
-result.write(f"Khan: {{:.3%}.format(khan_percentage) + " " + str(Khan_votes)}" + "\n")
-result.write(f"Khan: {{:.3%}.format(Correy_percentage) + " " + str(Khan_votes)}" + "\n")
-# result.write("Correy: "+"{:.3%}".format(Correy_percentage) + " " + str(Correy_votes))
-result.write(f"Khan: {{:.3%}.format(Li_percentage) + " " + str(Khan_votes)}" + "\n")
-# result.write("Li: "+"{:.3%}".format(Li_percentage) + " " + str(Li_votes))
-result.write(f"Khan: {{:.3%}.format(OTooley_percentage) + " " + str(Khan_votes)}" + "\n")
-# result.write("O'Tooley: "+"{:.3%}".format(OTooley_percentage) + " " + str(OTooley_votes))
-result.write("-------------------------------------------" + "\n")
-result.write("Winner: " + "Khan")
-result.write("-------------------------------------------" + "\n")
-
+csvpath = os.path.join('Resources', 'election_Data_Summ.txt')
+with open(csvpath, 'w')as text:
+    text.write("Election Results" + "\n")
+    text.write("-------------------------------------------" + "\n")
+    text.write("Total Votes: {str(total_votes)}" + "\n")
+    text.write("-------------------------------------------" + "\n")
+    text.write("Khan: {:.3%}.format(khan_percentage) + " " + str(Khan_votes)" + "\n")
+    text.write("Khan: {:.3%}.format(Correy_percentage) + " " + str(Khan_votes)" + "\n")
+    text.write("Khan: {:.3%}.format(Li_percentage) + " " + str(Khan_votes)" + "\n")
+    text.write("Khan: {:.3%}.format(OTooley_percentage) + " " + str(Khan_votes)" + "\n")
+    text.write("-------------------------------------------" + "\n")
+    text.write(f"Winner: {winner}")
+    text.write("-------------------------------------------" + "\n")
